60fa68fc262778e3facf5f632bc625ca198790fb
plavelo <plavelo@users.noreply.github.com>
2021-09-24 15:36:09 +0900

    Time: 28 ms (30.07%), Space: 3.8 MB (90.39%) - LeetHub

---
+++ two-sum/two-sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+	size := len(nums)
+	for i := 0; i < size-1; i++ {
+		for j := i + 1; j < size; j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	panic("not found")
+}
